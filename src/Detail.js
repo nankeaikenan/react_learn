50266c76583e070fc64941564cc944c1ab55ffca
chenjian <872481426@qq.com>
2022-03-28 21:36:27 +0800

    learn router

---
+++ src/Detail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Detail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>详情页</h1>
+                <button>回首页</button>
+            </div>
+        )
+    }
+}
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>首页</h1>
+                <button>去详情页</button>
+            </div>
+        )
+    }
+}
